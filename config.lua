d59ddd413f39b7923f6a6c9d1023984418c0e060
Bearylul <78507312+Bearylul@users.noreply.github.com>
2021-08-13 20:51:44 -0700

    Moved config.lua to client.lua

--- config.lua
+++
@@ -1,5 +0,0 @@
-TGIANN = {}
-
---location of the hud
-TGIANN.positionx = 0.0 -- -0.15 left of screen, 0.0 Side of the map , 0.3 Middle of the screen,  0.6 or 0.7 Right of screen
-TGIANN.positiony = 0.0 -- 0.0 Bottom of the screen, -0.3 Middle of the screen, -0.9 Top of the screen
