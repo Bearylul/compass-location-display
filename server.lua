81483f653cc37d510048d9da04e4dcb641dbebc2
Bearylul <78507312+Bearylul@users.noreply.github.com>
2021-08-13 20:46:07 -0700

    Removed

--- server.lua
+++
@@ -1,7 +0,0 @@
-RegisterServerEvent('tgiann-carhud:eject-other-player-car')
-AddEventHandler('tgiann-carhud:eject-other-player-car', function(table, velocity)
-    for i=1, #table do
-        TriggerClientEvent("tgiann-carhud:eject-other-player-car-client", table[i], velocity)
-    end
-end)
-
