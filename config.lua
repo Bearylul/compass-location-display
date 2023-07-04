bcca7730bd2db1b77f64cc332672dde452b93f9a
Marshular's Development <78507312+marshular@users.noreply.github.com>
2023-07-03 21:20:39 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,6 @@
 Config = {}
 
+Config.color = "~o~"
 Config.nearestPostal = "nearest-postal" -- name of your nearest postal resource
 Config.positionx = -0.006 -- -0.15 left of screen, 0.0 Side of the map , 0.3 Middle of the screen,  0.6 or 0.7 Right of screen
 Config.positiony = -0.02 -- 0.0 Bottom of the screen, -0.3 Middle of the screen, -0.9 Top of the screen
